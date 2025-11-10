e1b7290e965d21aea3e5fc349877d98a0872937f
Basi Cordoba Arcas <basiliocordobaarcas132@gmail.com>
2025-11-10 21:37:25 +0100

    v1

---
+++ js/main.js
@@ -0,0 +1,56 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-200px');
+        }
+    });
+
+
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+})(jQuery);
+// Spinner
+window.addEventListener('load', function () {
+    const spinner = document.getElementById('spinner');
+    if (spinner) {
+        spinner.classList.add('fade'); // Asumiendo que tienes clase fade en CSS
+        setTimeout(() => spinner.style.display = 'none', 500);
+    }
+});
+
+// Inicializar animaciones WOW
+new WOW().init();
+
+
+
